ad7a63e9a75f10f0f350764b9a717e97c9794720
Aaron Stroud <astroud@gmail.com>
2021-03-15 12:35:59 -0700

    chore: add dependency, lint rules, + route

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,30 @@ module.exports = {
     ecmaVersion: 12,
   },
   rules: {
+    indent: [
+      'error',
+      2,
+    ],
+    'linebreak-style': [
+      'error',
+      'unix',
+    ],
+    quotes: [
+      'error',
+      'single',
+    ],
+    semi: [
+      'error',
+      'never',
+    ],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always',
+    ],
+    'arrow-spacing': [
+      'error', { before: true, after: true },
+    ],
+    'no-console': 0,
   },
-};
+}
--- index.js
+++ index.js
@@ -1,46 +1,50 @@
-const http = require('http');
-const express = require('express');
+const http = require('http')
+const express = require('express')
 
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
 
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
   likes: Number,
-});
+})
 
-const Blog = mongoose.model('Blog', blogSchema);
+const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist';
+const mongoUrl = 'mongodb://localhost/bloglist'
 mongoose.connect(mongoUrl, {
   useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true,
-});
+})
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  response.send('Hi, I\'m the blog\'s Back End! Are you looking for the /api/blogs route?')
+})
 
 app.get('/api/blogs', (request, response) => {
   Blog
     .find({})
     .then((blogs) => {
-      response.json(blogs);
-    });
-});
+      response.json(blogs)
+    })
+})
 
 app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
+  const blog = new Blog(request.body)
 
   blog
     .save()
     .then((result) => {
-      response.status(201).json(result);
-    });
-});
+      response.status(201).json(result)
+    })
+})
 
-const PORT = 3003;
+const PORT = 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
